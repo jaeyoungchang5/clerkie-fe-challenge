974cc8c766933dd11c0660e74f187a1cf8aa8887
JaeYoung Chang <jaeyoungchang5@gmail.com>
2024-04-11 02:32:35 -0500

    Update function title and get rid of unnecessary extra setState

--- src/app/components/PaymentDetails.tsx
+++ src/app/components/PaymentDetails.tsx
@@ -14,37 +14,13 @@ const PaymentDetails = () => {
         { name: 'C', balance:  5438, isSelected: false, accountPayment: 0 },
     ]);
 
-    useEffect(() => {
-        calculateProrateAmounts(accounts)
-    }, [paymentAmount]);
-
     const totalBalance = accounts.reduce((sum, current) => sum + current.balance, 0);
 
-    function handleCheckedUpdate(name: string, isChecked: boolean) {
-        let subtotal: number = accounts.reduce((sum, current) => {
-            if (current.isSelected) return sum + current.balance 
-            return sum
-        }, 0);
+    useEffect(() => {
+        updateAccounts(accounts)
+    }, [paymentAmount]);
 
-        let updatedList: AccountDetails[] = accounts.map(account => {
-            if (account.name !== name) {
-                return account;
-            }
-            return {...account, isSelected: isChecked}
-        })
-        setAccounts(updatedList);
-        calculateProrateAmounts(updatedList);
-    }
-
-    function handlePaymentAmountChange(value: number) {
-        if (isNaN(value)) {
-            return;
-        }
-
-        setPaymentAmount(value);
-    }
-
-    function calculateProrateAmounts(accountsObj: AccountDetails[]) {
+    function updateAccounts(accountsObj: AccountDetails[]) {
         let subtotal: number = accountsObj.reduce((sum, current) => sum + (current.isSelected ? current.balance : 0), 0);
         let updatedList: AccountDetails[] = accountsObj.map(account => {
             if (!account.isSelected || !paymentAmount) {
@@ -54,6 +30,25 @@ const PaymentDetails = () => {
             }
         })
         setAccounts(updatedList);
+    }
+
+    function handleCheckedUpdate(name: string, isChecked: boolean) {
+        let updatedList: AccountDetails[] = accounts.map(account => {
+            if (account.name !== name) {
+                return account;
+            }
+            return {...account, isSelected: isChecked}
+        })
+        
+        updateAccounts(updatedList);
+    }
+
+    function handlePaymentAmountChange(value: number) {
+        if (isNaN(value)) {
+            return;
+        }
+
+        setPaymentAmount(value);
     }
 
     return (
