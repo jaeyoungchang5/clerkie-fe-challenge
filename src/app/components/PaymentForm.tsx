8695264c1f33d73c86054b34871f8192bfe5b654
JaeYoung Chang <jaeyoungchang5@gmail.com>
2024-04-11 23:31:58 -0500

    Add success page

--- src/app/components/PaymentForm.tsx
+++ src/app/components/PaymentForm.tsx
@@ -3,13 +3,16 @@ import React, { useState } from 'react'
 import PaymentInformation from './PaymentInformation'
 import PaymentDetails from './PaymentDetails'
 import Button from './Common/Button';
+import Success from './Common/Success';
 
 const PaymentForm = () => {
+	const [didSubmit, setDidSubmit] = useState<boolean>(false);
 	const [isPaymentInfoValid, setIsPaymentInfoValid] = useState<boolean>(true);
 	const [isPaymentDetailValid, setIsPaymentDetailValid] = useState<boolean>(true);
 
 	function submitForm(formData: FormData) {
 		console.log(formData);
+		setDidSubmit(true);
 	}
 
 	function updatePaymentInfoValidity(isValid: boolean) {
@@ -22,11 +25,15 @@ const PaymentForm = () => {
 
 	return (
 		<div className='flex border rounded-lg p-6 bg-white border-gray-500'>
-			<form action={submitForm} className='flex flex-col'>
-				<PaymentInformation updateValidity={updatePaymentInfoValidity} />
-				<PaymentDetails updateValidity={updatePaymentDetailValidity} />
-				<Button disabled={!isPaymentInfoValid || !isPaymentDetailValid} text='Submit' type='submit' />
-			</form>
+			{
+				!didSubmit ?
+				<form action={submitForm} className='flex flex-col'>
+					<PaymentInformation updateValidity={updatePaymentInfoValidity} />
+					<PaymentDetails updateValidity={updatePaymentDetailValidity} />
+					<Button disabled={!isPaymentInfoValid || !isPaymentDetailValid} text='Submit' type='submit' />
+				</form>
+				: <Success />
+			}
 		</div>
 	)
 }
