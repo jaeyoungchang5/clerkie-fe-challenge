7a9b7de7e0ef15cef559a8b2d1adcd1c5aa05165
JaeYoung Chang <jaeyoungchang5@gmail.com>
2024-04-12 11:44:34 -0500

    Minor account styling updates

--- src/app/components/Common/Input.tsx
+++ src/app/components/Common/Input.tsx
@@ -8,9 +8,11 @@ const Input = ({ name, label, defaultText, value, errorMessage, disabled = false
 
 	return (
 		<div className='grid grid-rows-4'>
-			<label className='row-span-1' htmlFor={label}>
-				{label}
-			</label>
+			{label && 
+				<label className='row-span-1' htmlFor={label}>
+					{label}
+				</label>
+			}
 			<input
 				name={name}
 				value={value}
